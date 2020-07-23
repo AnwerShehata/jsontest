8f611f20c335c6409059b35fa1c39a4870c9a922
anwershehata@gmail.com <a01060454069>
2020-07-23 12:11:02 +0300

    Done Api

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,24 @@
-import 'package:flutter/material.dart';
-import './Home.dart';
-
-void main() {
-  runApp(MyApp());
-}
+  import 'package:flutter/material.dart';
+  import './HomeApp.dart';
 
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
 
-class _MyAppState extends State<MyApp> {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
-    );
+  main() async {
+    runApp(MyApp());
   }
-}
+
+  class MyApp extends StatefulWidget {
+    @override
+    _MyAppState createState() => _MyAppState();
+  }
+
+  class _MyAppState extends State<MyApp> {
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+
+        home: HomeApp(),
+
+      );
+    }
+  }
